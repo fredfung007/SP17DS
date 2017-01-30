84ed2992428ef47a831e34ecbfb6942a504768b3
Fred Fung <fred@rubyalliance.com>
2017-01-30 18:51:02 -0500

    lab2 checkpoint

---
+++ lab2/src/Account.java
@@ -0,0 +1,44 @@
+class  Account {
+
+    double bal;
+    int accountNum;
+
+    Account(int a) {
+        bal = 0.0;
+        accountNum = a;
+    }
+
+    public Account(int a, double b) {
+        bal = b;
+        accountNum = a;
+    }
+
+    public void deposit(double sum) {
+        if (sum > 0) {
+            bal += sum;
+            System.out.println("Deposit Successful ..");
+        } else {
+            System.err.println("cannot deposit negative amount.");
+        }
+    }
+
+    public void withdraw(double sum) {
+        if (sum > 0) {
+            bal -= sum;
+            System.out.println("Withdrawl Successful ..");
+        } else
+            System.err.println("cannot withdraw negative amount.");
+    }
+
+    public double getBalance() {
+        return bal;
+    }
+
+    public double getAccountNumber() {
+        return accountNum;
+    }
+
+    public String print() {
+        return "Account " + accountNum + ": " + "balance = " + bal;
+    }
+}
