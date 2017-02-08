a4b15931c516cd81228e36610a3bb8d2460f491c
Fred Fung <fred@rubyalliance.com>
2017-02-08 15:12:46 -0500

    Add comments to combinations.

--- lab3/src/Combinations.java
+++ lab3/src/Combinations.java
@@ -1,21 +1,26 @@
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Combinations {
     public List<List<Integer>> combine(int n, int k) {
+        // Base case; [[1],[2],[3],...,[n]] & [[1,2,3,...,n]]
         if (k == n || k == 0) {
-            List<Integer> row = new LinkedList<>();
+            List<Integer> elem = new ArrayList<>();
             for (int i = 1; i <= k; ++i) {
-                row.add(i);
+                elem.add(i);
             }
-            return new LinkedList<>(Arrays.asList(row));
+            ArrayList<List<Integer>> base = new ArrayList<>();
+            base.add(elem);
+            return base;
         }
-        
+
+        // recursive call to combine method, Choose k-1 from n-1;
         List<List<Integer>> result = this.combine(n - 1, k - 1);
+        // Add n to all previous sub problem.
         for (List<Integer> temp : result){
             temp.add(n);
         }
+        // recursive call to combine method, Choose k from n-1;
         result.addAll(this.combine(n - 1, k));
         return result;
     }
