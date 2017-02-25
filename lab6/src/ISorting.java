5f9e2eded81bd4445b16132f8aaf741d949f89c7
Fred Fung <fred@rubyalliance.com>
2017-02-25 15:13:02 -0500

    Add lab6

---
+++ lab6/src/ISorting.java
@@ -0,0 +1,7 @@
+/**
+ * Created by fredf on 2/21/2017.
+ */
+public interface ISorting<T extends Comparable<? super T>> {
+    // Return the result from sorting the input array.
+    T[] sort(T[] elements);
+}
