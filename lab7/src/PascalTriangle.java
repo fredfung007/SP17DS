e21f18465b677a5eea52de241a060fb7b9e74e32
Ubuntu <fred@ra-access-server.iusweq04vinu1a1uh1tywo3wod.bx.internal.cloudapp.net>
2017-03-08 08:59:48 -0500

    Fixed Pascal Triangle to start from row 0 instead of row 1.

--- lab7/src/PascalTriangle.java
+++ lab7/src/PascalTriangle.java
@@ -3,7 +3,7 @@
  */
 public class PascalTriangle {
     private static void print(int n) {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print(pascal(i, j) + " ");
             }
